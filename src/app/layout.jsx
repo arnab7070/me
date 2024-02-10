5e7c418f4ee81d3a5b5706d77307e057ea399a57
Arnab Nandi <arnabnandi7074@gmail.com>
2024-02-10 18:42:27 +0530

    Changed meta property

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,9 +7,7 @@ export const metadata = {
   title: 'Arnab Nandi - Portfolio',
   description: 'I am a full stack web developer with a passion for creating interactive and responsive web applications. I have experience working with JavaScript, React, Next, Flutter, Redux, Node.js, Express, MySQL, MongoDB, Firebase, HTML, CSS, and Git. I am a...',
   author: 'Arnab Nandi',
-  openGraph: {
-    images: ['/https://portfolio-arnab7070.vercel.app/_next/image?url=%2Fimages%2Fhero-image.png&w=750&q=75'],
-  },
+  image: 'https://portfolio-arnab7070.vercel.app/_next/image?url=%2Fimages%2Fhero-image.png&w=750&q=75',
 }
 
 export default function RootLayout({ children }) {
