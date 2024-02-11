af5043a352ca847af7c281c5e00f97b3d4b20e5e
Arnab Nandi <arnabnandi7074@gmail.com>
2024-02-11 11:04:38 +0530

    Updated Resume Link

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -146,7 +146,7 @@ const ProjectsSection = () => {
           isSelected={tag === "Mobile"}
         />
       </div>
-      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-6">
         {filteredProjects.map((project, index) => (
           <motion.li
             key={index}
